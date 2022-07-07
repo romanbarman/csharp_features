d9b441a5f517568dcbc7f98240cb412ff86b82c7
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:08:17 +0300

    Added auto property initializers feature

---
+++ CSharp.Features.6.0/AutoPropertyInitializers.cs
@@ -0,0 +1,8 @@
+﻿namespace CSharp.Features._6._0
+{
+    class AutoPropertyInitializers
+    {
+        public string Id { get; } = Guid.NewGuid().ToString();
+        public int Count { get; set; } = 0;
+    }
+}
