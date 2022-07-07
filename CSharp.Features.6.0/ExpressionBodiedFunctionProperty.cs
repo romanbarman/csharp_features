c29ac8421974bd7e29453cfeb71f4ae75e5500cf
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 20:37:18 +0300

    Added expression bodied function property feature

---
+++ CSharp.Features.6.0/ExpressionBodiedFunctionProperty.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._6._0
+{
+    class ExpressionBodiedFunctionProperty
+    {
+        public void Run() => Console.WriteLine("Hello world!");
+
+        public string Name { get; } = "Vlada";
+        public int Age { get; } = 23;
+        public string Info => Name + " " + Age;
+    }
+}
