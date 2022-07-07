f7029bcd29077e15920c588708789e4bd7790bdc
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:24:16 +0300

    Added await in catch/finally block feature

---
+++ CSharp.Features.6.0/AwaitInCatchFinallyBlock.cs
@@ -0,0 +1,37 @@
+﻿namespace CSharp.Features._6._0
+{
+    class AwaitInCatchFinallyBlock
+    {
+
+        public async Task Run()
+        {
+            try
+            {
+                Method();
+            }
+            catch (Exception ex)
+            {
+                await HandleException(ex);
+            }
+            finally
+            {
+                await Finalize();
+            }
+        }
+
+        private Task Finalize()
+        {
+            return Task.CompletedTask;
+        }
+
+        private void Method()
+        {
+            throw new NotImplementedException();
+        }
+
+        private Task HandleException(Exception exception)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
