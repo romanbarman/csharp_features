605582ec02060aebbfb2f37be985a1dca143997d
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:05:33 +0300

    Added dictionary initializers feature

---
+++ CSharp.Features.6.0/DictionaryInitializers.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Features._6._0
+{
+    class DictionaryInitializers
+    {
+        public void Run()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                ["A"] = "A",
+                ["B"] = "B",
+            };
+        }
+    }
+}
