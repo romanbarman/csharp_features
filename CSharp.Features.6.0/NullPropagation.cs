2c7cf8f6c36dbd0f7aa1a5f7d7a7b0575fcec0aa
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:39:08 +0300

    Added null propagation feature

---
+++ CSharp.Features.6.0/NullPropagation.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp.Features._6._0
+{
+    class NullPropagation
+    {
+        public void Run(string line)
+        {
+            Console.WriteLine(line?.Length ?? 0);
+        }
+    }
+}
