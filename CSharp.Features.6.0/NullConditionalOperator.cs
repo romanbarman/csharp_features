0210a5ee5d553db9a96671ad1c879d7813afeca1
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:34:36 +0300

    Added null conditional operator feature

---
+++ CSharp.Features.6.0/NullConditionalOperator.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp.Features._6._0
+{
+    internal class NullConditionalOperator
+    {
+        public void Run(string line)
+        {
+            Console.WriteLine(line ?? "Hello world");
+        }
+    }
+}
