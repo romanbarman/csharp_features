4bc9eccbdf27bb6249292f3ea195cfe2bc3e5c61
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:01:02 +0300

    Added string interpolation feature

---
+++ CSharp.Features.6.0/StringInterpolation.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._6._0
+{
+    class StringInterpolation
+    {
+        public void Run()
+        {
+            var name = "Vladimir";
+            Console.WriteLine($"Hello {name}");
+        }
+    }
+}
