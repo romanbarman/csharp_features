c56274eb48ebdfb344a67f8ed659b9dc2fdd3d32
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 17:15:47 +0300

    Added name of expression feature

---
+++ CSharp.Features.6.0/NameofExpression.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._6._0
+{
+    class NameofExpression
+    {
+        public void Run(string line)
+        {
+            if (line == null)
+                throw new ArgumentNullException(nameof(line));
+        }
+    }
+}
