3219252b8725ad360630fbbda3400a8017e00188
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 20:59:15 +0300

    Added out variables feature

---
+++ CSharp.Features.7.0/OutVariables.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.Features._7._0
+{
+    class OutVariables
+    {
+        public void Run()
+        {
+            Initialize(out var x, out var y);
+            Console.WriteLine($"x = {x}, y = {y}");
+        }
+
+        private void Initialize(out int x, out int y)
+        {
+            x = 0;
+            y = 0;
+        }
+    }
+}
