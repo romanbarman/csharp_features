efd4d6d8d0821040125513a4aaa3e0b1b9d17322
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 20:49:42 +0300

    Added exception filtering feature

---
+++ CSharp.Features.6.0/ExceptionFiltering.cs
@@ -0,0 +1,36 @@
+﻿namespace CSharp.Features._6._0
+{
+    class ExceptionFiltering
+    {
+        public void Run()
+        {
+            Random random = new Random();
+            var randomExceptions = random.Next(400, 405);
+
+            try
+            {
+                throw new Exception(randomExceptions.ToString());
+            }
+            catch (Exception ex) when (ex.Message.Equals("400"))
+            {
+                Console.WriteLine("Bad Request");
+            }
+            catch (Exception ex) when (ex.Message.Equals("401"))
+            {
+                Console.WriteLine("Unauthorized");
+            }
+            catch (Exception ex) when (ex.Message.Equals("402"))
+            {
+                Console.Write("Payment Required");
+            }
+            catch (Exception ex) when (ex.Message.Equals("403"))
+            {
+                Console.Write("Forbidden");
+            }
+            catch (Exception ex) when (ex.Message.Equals("404"))
+            {
+                Console.Write("Not Found");
+            }
+        }
+    }
+}
