2bdbca40866eca3ec0d4e8fac2d7faeba8c98dfd
Raman Zanko <r.zanko@godeltech.com>
2022-07-07 15:50:03 +0300

    Added static types as using feature

---
+++ CSharp.Features.6.0/StaticTypesAsUsing.cs
@@ -0,0 +1,13 @@
+﻿using static System.Console;
+using static System.Math;
+
+namespace CSharp.Features._6._0
+{
+    class StaticTypesAsUsing
+    {
+        public void Run()
+        {
+            WriteLine(Pow(5, 2));
+        }
+    }
+}
