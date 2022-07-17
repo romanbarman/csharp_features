c2adf914e4111145d6def36c69590450e566557f
Raman Zanko <r.zanko@godeltech.com>
2022-07-17 11:16:29 +0300

    Added throw expressions feature

---
+++ CSharp.Features.7.0/ThrowExpressions.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp.Features._7._0
+{
+    class ThrowExpressions
+    {
+        private string message;
+
+        public ThrowExpressions(string message) => this.message = message ?? throw new ArgumentNullException(nameof(message));
+
+        public string Get()
+        {
+            return this.message.Length > 5 ? this.message : throw new Exception("No");
+        }
+
+        public string Get(int i) => throw new NotImplementedException();
+    }
+}
