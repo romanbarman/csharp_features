c20ae373df3769335aed220fd29d04531a442dbc
Raman Zanko <r.zanko@godeltech.com>
2022-07-17 11:26:49 +0300

    Added default literals feature

---
+++ CSharp.Features.7.1/DefaultLiterals.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp.Features._7._1
+{
+    class DefaultLiterals
+    {
+        public async Task Run()
+        {
+            var result = await Get();
+
+            var cancelToken = new CancellationTokenSource();
+            result = await Get(cancelToken.Token);
+        }
+
+        private Task<int> Get(CancellationToken token = default)
+        {
+            return Task.Run(() =>
+            {
+                token.ThrowIfCancellationRequested();
+                return 10;
+            });
+        }
+    }
+}
