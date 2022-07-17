9c15b399a9a3c0734cd0637691f37c63f5a68169
Raman Zanko <r.zanko@godeltech.com>
2022-07-17 11:07:26 +0300

    Added ref returns and locals feature

---
+++ CSharp.Features.7.0/RefReturnsAndLocals.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.Features._7._0
+{
+    class RefReturnsAndLocals
+    {
+        public void Run()
+        {
+            var array = new[] { 1, 2, 3 };
+            ref var result = ref GetByIndex(1, array);
+            result = 100;
+            Console.WriteLine(array[1]);//prints 100
+        }
+
+        private ref int GetByIndex(int index, int[] array)
+        {
+            return ref array[index];
+        }
+    }
+}
