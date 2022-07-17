a34a489bef44b44efd9ca513965397894c668c2d
Raman Zanko <r.zanko@godeltech.com>
2022-07-17 10:56:03 +0300

    Added local functions feature

---
+++ CSharp.Features.7.0/LocalFunctions.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp.Features._7._0
+{
+    class LocalFunctions
+    {
+        public void Run()
+        {
+            Console.WriteLine(Sum(10, 15));
+
+            int Sum(int a, int b)
+            {
+                return a + b;
+            }
+        }
+    }
+}
