0c437f3876e8364fab84002c3e93a1edf25ba99b
Raman Zanko <r.zanko@godeltech.com>
2022-07-17 11:10:13 +0300

    Added literal improvements feature

---
+++ CSharp.Features.7.0/LiteralImprovements.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp.Features._7._0
+{
+    class LiteralImprovements
+    {
+        public void Run()
+        {
+            var d = 123_456;
+            var x = 0xAB_CD_EF;
+            var b = 0b1010_1011_1100_1101_1110_1111;
+        }
+    }
+}
