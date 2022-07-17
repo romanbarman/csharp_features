d78f0b115f61e5c2621e9b4c9ef5ca7b4edbff53
Raman Zanko <r.zanko@godeltech.com>
2022-07-17 11:01:16 +0300

    Added expression bodied members feature

---
+++ CSharp.Features.7.0/ExpressionBodiedMembers.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp.Features._7._0
+{
+    class ExpressionBodiedMembers
+    {
+        private int i;
+
+        public ExpressionBodiedMembers(int i) => this.i = i;
+        ~ExpressionBodiedMembers() => this.i = 0;
+
+        public int I
+        {
+            get => this.i;
+            set => this.i = value;
+        }
+    }
+}
