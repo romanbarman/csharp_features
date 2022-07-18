7b807c2cd27ea5acdea009cf69f281134baa3511
Raman Zanko <r.zanko@godeltech.com>
2022-07-18 14:12:28 +0300

    Added inferred tuple element names feature

---
+++ CSharp.Features.7.1/InferredTupleElementNames.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Features._7._1
+{
+    class InferredTupleElementNames
+    {
+        public void Run()
+        {
+            var age = 25;
+            var word = "Hello";
+            var tuple = (age, word);
+
+            Console.WriteLine($"{tuple.age} {tuple.word}");
+        }
+    }
+}
