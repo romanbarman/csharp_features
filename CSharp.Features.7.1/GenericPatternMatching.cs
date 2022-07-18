266002b77f85a301d3cd89d183e69141b02d9149
Raman Zanko <r.zanko@godeltech.com>
2022-07-18 15:53:44 +0300

    Added generic pattern matching feature

---
+++ CSharp.Features.7.1/GenericPatternMatching.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.Features._7._1
+{
+    class GenericPatternMatching
+    {
+        public void Run()
+        {
+            Console.WriteLine(Get(new[] { 1 }));
+        }
+
+        public int Get<T>(T obj) => obj switch
+        {
+            Array _ => 1,
+            string str => str.Length,
+            null => throw new Exception("NULL"),
+            _ => 0,
+        };
+    }
+}
