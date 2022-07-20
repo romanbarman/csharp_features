60a6dd5e122822aad2c6a8b7ab91cd4c9ad15668
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 12:09:50 +0300

    Added non-trailing named arguments feature

---
+++ CSharp.Features.7.2/NonTailingNamedArguments.cs
@@ -0,0 +1,16 @@
+﻿namespace CSharp.Features._7._2
+{
+    class NonTailingNamedArguments
+    {
+
+        public void Run()
+        {
+            Test(10, bold: true, true);
+        }
+
+        private void Test(int number, bool bold = false, bool centered = false)
+        {
+
+        }
+    }
+}
