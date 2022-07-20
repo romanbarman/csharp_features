9211f0d2baeb428dc0f4061c2093b4f6ce7831b8
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 14:30:26 +0300

    Added in modifier on parameters feature

---
+++ CSharp.Features.7.2/InModifierOnParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp.Features._7._2
+{
+    class InModifierOnParameters
+    {
+        public void Run()
+        {
+            Console.WriteLine(Increase(5));
+        }
+
+        private int Increase(in int a)
+        {
+            return a + 1;
+        }
+    }
+}
