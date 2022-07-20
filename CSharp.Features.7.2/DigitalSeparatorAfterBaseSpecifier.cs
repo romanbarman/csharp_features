f4ee2e92958f3cace76582c80831fbe96606e106
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 12:04:40 +0300

    Added digital separator after base specifier feature

---
+++ CSharp.Features.7.2/DigitalSeparatorAfterBaseSpecifier.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._7._2
+{
+    class DigitalSeparatorAfterBaseSpecifier
+    {
+        public void Run()
+        {
+            var hex = 0x_ff_ff_ff;
+            var bin = 0b_0000_1111;
+        }
+    }
+}
