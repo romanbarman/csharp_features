f000019caf38fdd17c6499682eb8345e93ac3b18
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 12:16:34 +0300

    Added ref conditional expression feature

---
+++ CSharp.Features.7.2/RefConditionalExpression.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._7._2
+{
+    class RefConditionalExpression
+    {
+        public void Run()
+        {
+            var array = new[] { 1, 2, 3 };
+            ref var first = ref (array.Length < 2 ? ref array[0] : ref array[1]);
+        }
+    }
+}
