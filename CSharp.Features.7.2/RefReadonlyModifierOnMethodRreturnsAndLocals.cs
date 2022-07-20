dd665c7e41baddf212ff95e38c8a2bc61900472e
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 14:23:16 +0300

    Added ref readonly modifier on method returns and locals feature

---
+++ CSharp.Features.7.2/RefReadonlyModifierOnMethodRreturnsAndLocals.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.Features._7._2
+{
+    class RefReadonlyModifierOnMethodRreturnsAndLocals
+    {
+        public void Run()
+        {
+            var array = new[] { 1, 2, 3 };
+            ref readonly var result = ref GetByIndex(1, array);
+            Console.WriteLine(array[1]);
+        }
+
+        private ref readonly int GetByIndex(int index, int[] array)
+        {
+            return ref array[index];
+        }
+    }
+}
