ddabfd940f1aec28590f7f2a25b7e2a82f005bc8
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 16:57:41 +0300

    Added ref local reassignment feature

---
+++ CSharp.Features.7.3/RefLocalReassignment.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Features._7._3
+{
+    class RefLocalReassignment
+    {
+        public void Run()
+        {
+            var array = new[] { 1, 2, 3 };
+            ref var element = ref array[0];
+            element = ref array[1];
+
+            Console.WriteLine(element);
+        }
+    }
+}
