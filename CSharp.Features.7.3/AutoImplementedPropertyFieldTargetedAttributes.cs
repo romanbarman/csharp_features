3de0a645c73e98dd17c7f330681708c9f6e4483a
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 16:43:23 +0300

    Added auto-implemented property field targeted attributes feature

---
+++ CSharp.Features.7.3/AutoImplementedPropertyFieldTargetedAttributes.cs
@@ -0,0 +1,9 @@
+﻿namespace CSharp.Features._7._3
+{
+    [Serializable]
+    class AutoImplementedPropertyFieldTargetedAttributes
+    {
+        [field: NonSerialized]
+        public string Secret { get; set; }
+    }
+}
