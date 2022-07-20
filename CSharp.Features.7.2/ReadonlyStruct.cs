b645c721a62dcff4d0e5d08ef287a0dc85c58bab
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 14:35:41 +0300

    Added readonly struct feature

---
+++ CSharp.Features.7.2/ReadonlyStruct.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp.Features._7._2
+{
+    class ReadonlyStruct
+    {
+        public void Run()
+        {
+            var test = new TestStruct(10);
+            Show(test);
+        }
+
+        private void Show(in TestStruct testStruct)
+        {
+            Console.WriteLine(testStruct);
+        }
+    }
+
+    readonly struct TestStruct
+    {
+        private readonly int x;
+
+        public TestStruct(int x)
+        {
+            this.x = x;
+        }
+    }
+}
