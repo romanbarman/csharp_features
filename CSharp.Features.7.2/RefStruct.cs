b862afce40b59f86c6723832d1de005e1b043df3
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 14:15:55 +0300

    Added ref struct feature

---
+++ CSharp.Features.7.2/RefStruct.cs
@@ -0,0 +1,20 @@
+﻿
+namespace CSharp.Features._7._2
+{
+    class RefStruct
+    {
+        public void Run()
+        {
+            var test = new Test(10);
+        }
+    }
+
+    ref struct Test
+    {
+        private int x;
+        public Test(int x)
+        {
+            this.x = x;
+        }
+    }
+}
