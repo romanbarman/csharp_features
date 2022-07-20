0aba480e3198b66ba220db433bba7c1f8d600efd
Raman Zanko <r.zanko@godeltech.com>
2022-07-20 17:14:39 +0300

    Added tuple equality feature

---
+++ CSharp.Features.7.3/TupleEquality.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Features._7._3
+{
+    class TupleEquality
+    {
+        public void Run()
+        {
+            var t1 = (25, "Hi");
+            var t2 = (25, "Yes");
+
+            Console.WriteLine(t1 == t2);
+            Console.WriteLine(t1 != t2);
+        }
+    }
+}
