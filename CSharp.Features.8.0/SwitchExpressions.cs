53fdf3ca66e22b3cf4d8d5f23660a8d3ae3639f2
Raman Zanko <r.zanko@godeltech.com>
2022-07-21 14:59:05 +0300

    Added switch expressions feature

---
+++ CSharp.Features.8.0/SwitchExpressions.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.Features._8._0
+{
+    class SwitchExpressions
+    {
+        public void Run()
+        {
+            Console.WriteLine(Get("Hi"));
+        }
+
+        private int Get(object obj) => obj switch
+        {
+            string _ => 0,
+            IEnumerable<int> _ => 1,
+            _ => throw new ArgumentException("No")
+        };
+    }
+}
