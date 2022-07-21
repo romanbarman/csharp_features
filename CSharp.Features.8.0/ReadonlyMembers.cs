99382200c776ab572f33071dd7278e01a7acb15d
Raman Zanko <r.zanko@godeltech.com>
2022-07-21 14:02:22 +0300

    Added readonly members feature

---
+++ CSharp.Features.8.0/ReadonlyMembers.cs
@@ -0,0 +1,26 @@
+﻿namespace CSharp.Features._8._0
+{
+    class ReadonlyMembers
+    {
+        public void Run()
+        {
+            var test = new TestStruct(5);
+            Console.WriteLine(test);
+        }
+    }
+
+    struct TestStruct
+    {
+        public int Value { get; set; }
+        public readonly int Square => Value * Value;
+        public TestStruct(int value)
+        {
+            Value = value;
+        }
+
+        public readonly override string ToString()
+        {
+            return Value.ToString() + Square.ToString();
+        }
+    }
+}
