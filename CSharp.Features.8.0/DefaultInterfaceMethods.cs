5d03835e40ebc60ca3fb3375aab60c795bccc76f
Raman Zanko <r.zanko@godeltech.com>
2022-07-21 14:27:04 +0300

    Added default interface methods feature

---
+++ CSharp.Features.8.0/DefaultInterfaceMethods.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp.Features._8._0
+{
+    class DefaultInterfaceMethods
+    {
+        public void Run()
+        {
+            IWrite<int> write = new Write();
+            write.Write(10);
+        }
+    }
+
+    interface IWrite<T>
+    {
+        void Write(T value)
+        {
+            Console.WriteLine(value);
+        }
+    }
+
+    class Write : IWrite<int>
+    {
+
+    }
+}
