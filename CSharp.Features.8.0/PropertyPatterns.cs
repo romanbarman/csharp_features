7ac16314567f52c74c7574b8d00df90075016cc0
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 10:41:01 +0300

    Added property patterns feature

---
+++ CSharp.Features.8.0/PropertyPatterns.cs
@@ -0,0 +1,24 @@
+﻿namespace CSharp.Features._8._0
+{
+    class PropertyPatterns
+    {
+        public void Run()
+        {
+            var person = new Person { Name = "Kiril" };
+            person.Id = GetId(person);
+        }
+
+        public static int GetId(Person person) => person switch
+        {
+            { Name: "Kiril" } => 1,
+            { Name: "Alexander" } => 2,
+            _ => 3
+        };
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
