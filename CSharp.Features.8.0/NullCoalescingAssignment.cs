57961d721e801004dc27f4a3db1f2aa11ede2a8f
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 11:29:12 +0300

    Added null-coalescing assignment feature

---
+++ CSharp.Features.8.0/NullCoalescingAssignment.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._8._0
+{
+    class NullCoalescingAssignment
+    {
+        public void Run()
+        {
+            int? i = null;
+            i ??= 15;
+        }
+    }
+}
