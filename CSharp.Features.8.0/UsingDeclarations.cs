5866ba536d195c557c75fddc5596774113ab4e66
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 10:56:53 +0300

    Added using declarations feature

---
+++ CSharp.Features.8.0/UsingDeclarations.cs
@@ -0,0 +1,14 @@
+﻿namespace CSharp.Features._8._0
+{
+    class UsingDeclarations
+    {
+        public void Run()
+        {
+            using var file = new StreamWriter("Text.txt");
+            for (int i = 0; i < 100; i++)
+            {
+                file.WriteLine(i);
+            }
+        }
+    }
+}
