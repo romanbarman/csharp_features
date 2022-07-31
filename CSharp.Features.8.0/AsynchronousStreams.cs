1a88fa0a128a307f6fa9412a9ee234129da09af3
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 11:16:37 +0300

    Added asynchronous streams feature

---
+++ CSharp.Features.8.0/AsynchronousStreams.cs
@@ -0,0 +1,22 @@
+﻿namespace CSharp.Features._8._0
+{
+    class AsynchronousStreams
+    {
+        public async Task Run()
+        {
+            await foreach (var item in Get())
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static async IAsyncEnumerable<int> Get()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                await Task.Delay(100);
+                yield return i;
+            }
+        }
+    }
+}
