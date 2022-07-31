7f303f4ec83ed36e22f156a32879b4e75366686b
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 10:45:38 +0300

    Added tuple patterns feature

---
+++ CSharp.Features.8.0/TuplePatterns.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.Features._8._0
+{
+    class TuplePatterns
+    {
+        public void Run()
+        {
+            Console.WriteLine(GetNumber(2, "Akexander"));
+        }
+
+        public static int GetNumber(int id, string name) => (id, name) switch
+        {
+            (1, "Kiril") => 0,
+            (2, "Alexander") => 1,
+            _ => 3
+        };
+    }
+}
