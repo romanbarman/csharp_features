6c24f033815f9af2a3def6397176f9518d5d4e07
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 10:53:07 +0300

    Added positional patterns feature

---
+++ CSharp.Features.8.0/PositionalPatterns.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharp.Features._8._0
+{
+    class PositionalPatterns
+    {
+        public void Run()
+        {
+            Console.WriteLine(GetNumber(new Point(1, 2)));
+        }
+
+        public static int GetNumber(Point point) => point switch
+        {
+            (0, 0) => 0,
+            var (x, y) when x > 0 && y > 0 => 1,
+            var (x, y) when x > 0 && y < 0 => 2,
+            _ => 3
+        };
+    }
+
+    class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Deconstruct(out int x, out int y) => (x, y) = (X, Y);
+    }
+}
