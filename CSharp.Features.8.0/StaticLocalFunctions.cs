059f7c26fe7dc398a8035034753b7cfb97588647
Raman Zanko <r.zanko@godeltech.com>
2022-07-31 11:01:41 +0300

    Added static local functions feature

---
+++ CSharp.Features.8.0/StaticLocalFunctions.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.Features._8._0
+{
+    class StaticLocalFunctions
+    {
+        public void Run()
+        {
+            var x = 5;
+            var y = 10;
+
+            Console.WriteLine(Add(x, y));
+
+            static int Add(int right, int left)
+            {
+                return right + left;
+            }
+        }
+    }
+}
