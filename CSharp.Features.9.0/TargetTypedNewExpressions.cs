207af1eccc939a24eb47f39c15251f1db395cd2c
Raman Zanko <r.zanko@godeltech.com>
2022-08-01 16:53:12 +0300

    Added target-typed new expressions feature

---
+++ CSharp.Features.9.0/TargetTypedNewExpressions.cs
@@ -0,0 +1,12 @@
+﻿namespace CSharp.Features._9._0
+{
+    class TargetTypedNewExpressions
+    {
+        public void Run()
+        {
+            Dictionary<string, List<int>> field = new() {
+                { "item1", new() { 1, 2, 3 } }
+            };
+        }
+    }
+}
