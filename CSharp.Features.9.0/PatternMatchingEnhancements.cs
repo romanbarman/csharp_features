a232ca48a1c3f8323392dd6718b044a3cc24a145
Raman Zanko <r.zanko@godeltech.com>
2022-08-01 16:42:18 +0300

    Added pattern matching enhancements feature

---
+++ CSharp.Features.9.0/PatternMatchingEnhancements.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharp.Features._9._0
+{
+    class PatternMatchingEnhancements
+    {
+        public void Run()
+        {
+            var str = "Hi";
+
+            foreach (var ch in str)
+            {
+                if (ch is (>= 'a' and <= 'z') or (>= 'A' and <= 'Z'))
+                {
+                    Console.WriteLine(ch);
+                }
+            }
+        }
+    }
+}
