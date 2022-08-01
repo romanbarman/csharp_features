1340a67ea80d14adaec71f7fb5dfa7b4f6ac7a1b
Raman Zanko <r.zanko@godeltech.com>
2022-08-01 12:31:47 +0300

    Added init only setters feature

---
+++ CSharp.Features.9.0/InitOnlySetters.cs
@@ -0,0 +1,15 @@
+﻿namespace CSharp.Features._9._0
+{
+    class InitOnlySetters
+    {
+        public void Run()
+        {
+            var test = new Test { Id = 1 };
+        }
+    }
+
+    class Test
+    {
+        public int Id { get; init; }
+    }
+}
