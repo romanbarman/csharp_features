e7261239227ce26c00a0bcbcf66172d70324c3b8
Raman Zanko <r.zanko@godeltech.com>
2022-08-17 10:35:46 +0300

    Added record structs feature

---
+++ CSharp.Features.10.0/RecordStructs.cs
@@ -0,0 +1,17 @@
+﻿namespace CSharp.Features._10._0
+{
+    class RecordStructs
+    {
+        public void Run()
+        {
+            var point = new Point() { X = 10, Y = 20, Z = 30 };
+        }
+    }
+
+    public record struct Point
+    {
+        public double X { get; init; }
+        public double Y { get; init; }
+        public double Z { get; init; }
+    }
+}
