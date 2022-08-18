89d26e254487af27e452bbb7c0e48b9faf29523c
Raman Zanko <r.zanko@godeltech.com>
2022-08-18 14:48:29 +0300

    Added natural type of a lambda expression feature

---
+++ CSharp.Features.10.0/NaturalTypeOfALambdaExpression.cs
@@ -0,0 +1,11 @@
+﻿namespace CSharp.Features._10._0
+{
+    class NaturalTypeOfALambdaExpression
+    {
+        public void Run()
+        {
+            var parse = (string s) => int.Parse(s);
+            var action = () => Console.WriteLine(parse);
+        }
+    }
+}
